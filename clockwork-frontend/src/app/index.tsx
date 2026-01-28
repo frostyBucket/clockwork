d2f7c6050a808b9677b906689c0cac48e56efb95
frostyBucket <devking2022@gmail.com>
2026-01-28 08:31:51 -0600

    little bit of a start :D

---
+++ clockwork-frontend/src/app/index.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+import {Stack} from "expo-router";
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>Dude</Text>
+        </View>
+    )
+}
